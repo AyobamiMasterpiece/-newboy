022407ed5e54d4670e73f978c6f533580a18f8e5
AyobamiMasterpiece <ayotibami14@gmail.com>
2022-10-22 07:39:23 +1300

    l

--- teens/App.js
+++ teens/App.js
@@ -1,69 +1,449 @@
-import React, { useRef, useState } from "react";
-import { Button, DrawerLayoutAndroid, Text, StyleSheet, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, TextInput, TouchableNativeFeedback, View } from 'react-native';
 
-const App = () => {
-  const drawer = useRef(null);
-  const [drawerPosition, setDrawerPosition] = useState("left");
-  const changeDrawerPosition = () => {
-    if (drawerPosition === "left") {
-      setDrawerPosition("right");
-    } else {
-      setDrawerPosition("left");
-    }
-  };
+export default function App() {
 
-  const navigationView = () => (
-    <View style={[styles.container, styles.navigationContainer]}>
-      <Text style={styles.paragraph}>I'm in the Drawer!</Text>
-      <Button
-        title="Close drawer"
-        onPress={() => drawer.current.closeDrawer()}
-      />
+  const[ripple,setripple]=useState(false);
+  const[text,settext]=useState('')
+  const array=[]
+  return (
+    <View style={styles.container}>
+      <View style={styles.cac}>
+        <View style ={{backgroundColor:'red', flex:0.3}}> 
+        
+        <Text style={{fontSize:32}}>{text}</Text>
+        
+        </View>
+       
+      </View>
+    
+      <StatusBar  />
+      <View style={styles.press}>
+
+
+      
+      <View style= {styles.rows}>
+  <TouchableNativeFeedback
+  onPress={()=>{
+    settext('')
+    setripple(!ripple)
+  }}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>C</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+     
+      <TouchableNativeFeedback
+  onPress={()=>{ settext(text/text)
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>/</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text+'*')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>*</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text +'/')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={[styles.text]}>X</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+
+
+
+
+
+      </View>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+<View style= {styles.rows}>
+  <TouchableNativeFeedback
+  onPress={()=>{settext(text+'7')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>7</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+     
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text+'8')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>8</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text+'9')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>9</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+      <TouchableNativeFeedback
+  onPress={()=>{setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={[styles.text]}>-</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+
+
+
+
+
+      </View>
+
+
+
+
+
+
+
+
+
+
+      
+      <View style= {styles.rows}>
+  <TouchableNativeFeedback
+  onPress={()=>{settext(text+'4')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>4</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+     
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text+'5')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>5</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text+'6')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>6</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text+'+')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={[styles.text]}>+</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+
+
+
+
+
+      </View>
+
+
+
+      
+      <View style= {styles.rows}>
+  <TouchableNativeFeedback
+  onPress={()=>{settext(text+'1')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>1</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+     
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text+'2')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>2</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text+'3')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={[styles.text,{marginLeft:6}]}>3</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+
+<View  style={{backgroundColor:'black',marginLeft:35,padding:16,marginTop:5,borderRadius:10,paddingBottom:29}}>
+<TouchableNativeFeedback
+  onPress={()=>{settext(text+'=')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('pink',ripple)}
+  
+  >
+    
+    <Text style={styles.text}>=</Text>
+    
+      </TouchableNativeFeedback>
+
+</View>
+
+
+      </View>
+
+
+
+
+
+
+
+
+
+      
+      <View style= {styles.rows}>
+  <TouchableNativeFeedback
+  onPress={()=>{settext(text+'%')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>%</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+     
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text+'0')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>0</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+      <TouchableNativeFeedback
+  onPress={()=>{settext(text+'.')
+    setripple(!ripple)}}
+  background={TouchableNativeFeedback.Ripple('black',ripple)}
+  
+  >
+    <View style={styles.textcontainer}>
+    <Text style={styles.text}>.</Text>
+      </View>  
+      </TouchableNativeFeedback>
+
+
+
+
+      </View>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </View>
+     
     </View>
   );
-
-  return (
-    <DrawerLayoutAndroid
-      ref={drawer}
-      drawerWidth={300}
-      drawerPosition={drawerPosition}
-      renderNavigationView={navigationView}
-    >
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Drawer on the {drawerPosition}!
-        </Text>
-        <Button
-          title="Change Drawer Position"
-          onPress={() => changeDrawerPosition()}
-        />
-        <Text style={styles.paragraph}>
-          Swipe from the side or press button below to see it!
-        </Text>
-        <Button
-          title="Open drawer"
-          onPress={() => drawer.current.openDrawer()}
-        />
-      </View>
-    </DrawerLayoutAndroid>
-  );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 16
+    backgroundColor:'pink'
+    
+    
   },
-  navigationContainer: {
-    backgroundColor: "#ecf0f1"
+  cac:{
+flex:1,
+backgroundColor:'white',
+justifyContent:'flex-end'
+
   },
-  paragraph: {
-    padding: 16,
-    fontSize: 15,
-    textAlign: "center"
+  press:{
+    flex:1.3,
+    backgroundColor:'darkblue',
+    
+    
+  },
+  text:{
+    fontSize:24,
+    color:'white'
+    
+  },
+  textcontainer:{
+margin:25,
+marginLeft:35
+
+  },
+  rows:{
+    flexDirection:'row'
   }
 });
-
-export default App;
