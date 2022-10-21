78cef1d79b48a8ece14551e3fa42fa9e2c828a9d
AyobamiMasterpiece <ayotibami14@gmail.com>
2022-10-22 05:31:15 +1300

    l

---
+++ jsproject/index.js
@@ -0,0 +1,37 @@
+// let button = document.getElementById('input-btn')
+// console.log(button)
+// button.addEventListener("click",()=>console.log('click'))
+
+
+
+
+l
+console.log(message)
+let boy=message.p
+message[0].textContent='hey'
+
+
+ let arraylist=[3,4,5]
+
+  function array(){
+    document.getElementById('grace').textContent= 'K'
+    console.log( document.getElementById('grace').textContent)
+    arraylist.push(4)
+
+    for (let index = 0; index < arraylist.length; index++) {
+        document.getElementById('grace')+=arraylist[index]
+        
+    }
+}
+  function startgame(){
+    alert ('ba')
+  }
+
+ let masterpiece= ['rock' ,'paper','scissor']
+ let ann= ['rock' ,'paper','scissor']
+ 
+ let number=Math.floor(Math.random()*3)
+ let num=Math.floor(Math.random()*3)
+ console.log(ann[number],masterpiece[num])
+
+
